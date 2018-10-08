9d34039b2167143a9673fb0d6ab6037c46957c3b
Letiteuf55 <arthur.lemoine@me.com>
2018-10-08 18:02:47 +0200

    Update to 1.1
    Add sections, add Twitter and Instagram support

--- UtipDarkMode.user.js
+++ UtipDarkMode.user.js
@@ -1,20 +1,52 @@
 // ==UserScript==
-// @name         Dark Theme for Utip
+// @name         BETA Dark Theme for Utip
 // @namespace    https://utip.io
-// @version      01
+// @version      1.1
 // @description  Just an other black theme
 // @author       CrazypandaFR
 // @match        https://utip.io/*
 // @grant        GM_addStyle
+
 // ==/UserScript==
 
+// Background
 
+GM_addStyle ('.ufeed-content { background-color: #1A1A1A !important; }');
 
+// Recommended Section
 
-GM_addStyle ('.ufeed-menu { background-color: black !important; }');
-GM_addStyle ('.ufeed-content { background-color: black !important; }');
-GM_addStyle ('.filter-title { color: white !important; }');
-GM_addStyle ('.nav-item { color: white !important; }');
-GM_addStyle ('.nav-link { color: white !important; }');
-GM_addStyle ('.level-indicator { color: white !important; }');
-GM_addStyle ('.menu-counter-item { color: white !important; }');
+GM_addStyle ('.recommended-container { background-color: #494949 !important; }');
+GM_addStyle ('.creator-recommended { background-color: #1A1A1A !important; }');
+
+GM_addStyle ('.recommended-title { color: #B7B7B7 !important; }');
+GM_addStyle ('.recommended-creator-name { color: #B7B7B7 !important; }');
+
+// Post Section
+
+GM_addStyle ('.post { background-color: #494949 !important; }');
+GM_addStyle ('.author-name { color: #B7B7B7 !important; }');
+GM_addStyle ('.date-text { color: #B7B7B7 !important; }');
+GM_addStyle ('.post-utip-text { color: #494949 !important; }');
+GM_addStyle ('.supportBtn { background-color: #FFFFFF !important; }');
+GM_addStyle ('.supportBtn { border-radius: 25px !important; }');
+
+// Menu Section
+
+GM_addStyle ('.ufeed-menu { background-color: #1A1A1A !important; }');
+GM_addStyle ('.filter-title { color: #B7B7B7 !important; }');
+GM_addStyle ('.nav-item { color: #B7B7B7 !important; }');
+GM_addStyle ('.nav-link { color: #B7B7B7 !important; }');
+GM_addStyle ('.level-indicator { color: #B7B7B7 !important; }');
+GM_addStyle ('.menu-counter-item { color: #B7B7B7 !important; }');
+
+// Utip
+
+GM_addStyle ('.post-content { background-color: #B7B7B7 !important; }');
+
+//Tweet
+
+GM_addStyle ('.EmbeddedTweet { background-color: #B7B7B7 !important; }');
+
+//Instagram
+
+GM_addStyle ('.EmbedInsta { background-color: #B7B7B7 !important; }');
