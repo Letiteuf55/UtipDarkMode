13ad1553194bf71b89d22e070673e4f7fff63ff0
Letiteuf55 <arthur.lemoine@me.com>
2018-10-08 14:35:08 +0200

    Create UtipDarkMode.js

---
+++ UtipDarkMode.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Dark Theme for Utip
+// @namespace    https://utip.io
+// @version      01
+// @description  Just an other black theme
+// @author       CrazypandaFR
+// @match        https://utip.io/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+
+
+
+GM_addStyle ('.ufeed-menu { background-color: black !important; }');
+GM_addStyle ('.ufeed-content { background-color: black !important; }');
+GM_addStyle ('.filter-title { color: white !important; }');
+GM_addStyle ('.nav-item { color: white !important; }');
+GM_addStyle ('.nav-link { color: white !important; }');
+GM_addStyle ('.level-indicator { color: white !important; }');
+GM_addStyle ('.menu-counter-item { color: white !important; }');
