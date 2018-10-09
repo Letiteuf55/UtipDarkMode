2690ee5314fac7033f33deb91eec0a24b656f2be
AlanGueite <alangueite@alangueite.ml>
2018-10-09 14:31:53 +0200

    Update UtipDarkMode.user.js

--- UtipDarkMode.user.js
+++ UtipDarkMode.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Dark Theme for Utip
 // @namespace    https://utip.io
-// @version      1.1
+// @version      1.0
 // @description  Just an other black theme
-// @author       CrazypandaFR
+// @author       CrazypandaFR, Shelp
 // @match        https://utip.io/*
 // @grant        GM_addStyle
 
@@ -12,6 +12,7 @@
 // Background
 
 GM_addStyle ('.ufeed-content { background-color: #1A1A1A !important; }');
+GM_addStyle ('body { background-color: #1A1A1A !important; }');
 
 // Recommended Section
 
@@ -26,27 +27,65 @@ GM_addStyle ('.recommended-creator-name { color: #B7B7B7 !important; }');
 GM_addStyle ('.post { background-color: #494949 !important; }');
 GM_addStyle ('.author-name { color: #B7B7B7 !important; }');
 GM_addStyle ('.date-text { color: #B7B7B7 !important; }');
-GM_addStyle ('.post-utip-text { color: #494949 !important; }');
+GM_addStyle ('.post-utip-text { color: #bfbfbf !important; }');
 GM_addStyle ('.supportBtn { background-color: #FFFFFF !important; }');
 GM_addStyle ('.supportBtn { border-radius: 25px !important; }');
 
 // Menu Section
 
-GM_addStyle ('.ufeed-menu { background-color: #1A1A1A !important; }');
+GM_addStyle ('.ufeed-menu { background-color: #262626 !important; }');
 GM_addStyle ('.filter-title { color: #B7B7B7 !important; }');
 GM_addStyle ('.nav-item { color: #B7B7B7 !important; }');
 GM_addStyle ('.nav-link { color: #B7B7B7 !important; }');
 GM_addStyle ('.level-indicator { color: #B7B7B7 !important; }');
 GM_addStyle ('.menu-counter-item { color: #B7B7B7 !important; }');
+GM_addStyle ('.filter-search-bar-creators { background-color: #4d4d4d !important; }');
+GM_addStyle ('.filter-search-bar-creators input { color: #ffffff !important; }');
 
 // Utip
 
-GM_addStyle ('.post-content { background-color: #B7B7B7 !important; }');
+GM_addStyle ('.post-content { background-color: #595959 !important; }');
 
 //Tweet
 
-GM_addStyle ('.EmbeddedTweet { background-color: #B7B7B7 !important; }');
+GM_addStyle ('.EmbeddedTweet { background-color: #595959 !important; }');
 
 //Instagram
 
-GM_addStyle ('.EmbedInsta { background-color: #B7B7B7 !important; }');
+GM_addStyle ('.EmbedInsta { background-color: #595959 !important; }');
+
+//Login & Register
+
+GM_addStyle ('.bloc-register-infos { background-color: #262626 !important; }');
+GM_addStyle ('.bloc-register-tip { background-color: #1A1A1A !important; }');
+GM_addStyle ('.bloc-register-tip-footer { background-color: #0d0d0d !important; }');
+GM_addStyle ('.random-citations { color: #ffffff !important; }');
+GM_addStyle ('.bloc-register-infos { color: #FFFFFF !important; }');
+GM_addStyle ('.bloc-register-infos h2 { color: #FFFFFF !important; }');
+GM_addStyle ('.animate-input register-form-mail { color: #ffffff !important; }');
+
+//Password
+
+GM_addStyle ('.center-box { background-color: #0d0d0d !important; }');
+GM_addStyle ('.center-box h2 { color: #ffffff !important; }');
+GM_addStyle ('.center-box p { color: #ffffff !important; }');
+
+//User Feed
+
+GM_addStyle ('.ufeed-box { color: #ffffff !important; }');
+GM_addStyle ('.ufeed-box { background-color: #494949 !important; }');
+GM_addStyle ('.stats-block-amount { color: #ffffff !important; }');
+GM_addStyle ('.ufeed-input { color: #ffffff !important; }');
+GM_addStyle ('.ufeed-input { background-color: #4d4d4d !important; }');
+GM_addStyle ('.post-filter { background-color: #4d4d4d !important; }');
+
+//Creator Dashboard
+
+GM_addStyle ('.text-size-22 { color: #ffffff !important; }');
+GM_addStyle ('.text-size-14 { color: #ffffff !important; }');
+GM_addStyle ('.text-size-18 { color: #ffffff !important; }');
+GM_addStyle ('.m-auto { color: #000000 !important; }');
+
+//Beta
+
+GM_addStyle ('.feedback-btn p { color: #ffffff !important; }');
